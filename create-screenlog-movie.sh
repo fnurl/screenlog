f343750c9189041869883ee9ef7b3ffba9992d53
Jody Foo <jody.foo@gmail.com>
2014-07-11 20:42:57 +0200

    Cleaned up create-movie.sh. Can now specify date and remove jpgs automatically after creating the movie.

--- create-screenlog-movie.sh
+++ create-screenlog-movie.sh
@@ -1,50 +1,28 @@
 #!/bin/bash
 
-# This script is part of screenlog <http://>
+# This script is part of screenlog <https://github.com/fnurl/screenlog>
 #
 # This script creates a movie using todays screenshots. The directory
-# for todays screenshots is directory <screenlog path>/YY-MM-DD. Where
+# for todays screenshots is directory <screenlog path>/YYYY-MM-DD. Where
 # YY-MM-DD is today's date.
 #
-# The screenlog path is passed to the script as its argument.
+# The screenlog path is passed to the script as its argument. Use the -d option
+# select another date than today.
+#
+# Use the -r flag to remove the jpegs after creating the movie.
 #
 # Author: Jody Foo <jody.foo@gmail.com>
-# Date: 2014-05-22
+# Date: 2014-07-11
 
-# default values
-date=$(date +%Y-%m-%d)
-clear=""
-
-# extract options to variables
-while getopts ":d:c" options; do
-    case "${options}" in
-        d)
-            date=$2
-            ;;
-        c)
-            clear=1
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-shift $((OPTIND-1));
-
-echo "Date set to" $date
-echo ARGS: $1
-
-### TODO ###
-# * Check number of args
-
-# add path to ffmpeg
+# EDIT THIS LINE SO IT POINTS TO ffmpeg ON YOUR SYSTEM
 PATH=${PATH}:/usr/local/bin
 
 function usage() {
-    echo -e "Usage: `basename $0` <path to screenlog dir> [-d YY-MM-DD] [-c]"
-    echo -e "Creates a movie of the screencaptures in _todays_ log dir unless"
-    echo -e "the -d <YY-MM-DD> option is used."
-    echo -e "Using the -c flag deletes the jpgs after the movie has been created."
+    echo "Usage: `basename $0` [-d YYYY-MM-DD] [-r] <path to screenlog dir>"
+    echo "Create a movie of the screencaptures in _todays_ log dir unless the -d is used."
+    echo ""
+    echo "    -d <YYYY-MM-DD>  create movie for a specific date"
+    echo "    -r deletes the jpgs after the movie has been created"
 }
 
 function log() {
@@ -53,18 +31,49 @@ function log() {
     echo $message >> ~/Library/Logs/se.fnurl.createScreenlogMovie.log
 }
 
+# default values
+date=$(date +%Y-%m-%d)
+remove=""
+
+# extract options to variables
+while getopts ":d:r" options; do
+    case "${options}" in
+        d)
+            date=$2
+            ;;
+        r)
+            remove=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1));
+
+# After processing flags and options, check for required arguments.
+EXPECTED_ARGS=1
+E_BADARGS=65
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  usage
+  exit $E_BADARGS
+fi
+
+echo "Date set to" $date
+#echo ARGS: $1
+
+# Remove trailing / from path if it exists and append date to logpath
 logpath=${1%/}/$date
 mkdir -p $logpath
-
 echo LOGPATH: $logpath
-#exit 0
 
 # check for ffmpeg
 if hash ffmpeg 2>/dev/null; then
     log "ffmpeg found, creating movie..."
     ffmpeg -r 15 -pattern_type glob -i "$logpath/*.jpg" -vcodec libx264 $logpath/$date.mp4
     log "screenlog movie $logpath/$date.mp4 created."
-    if [ -n "$clear" ]; then
+    if [ -n "$remove" ]; then
         echo "Deleting source jpgs..."
         rm $logpath/*.jpg
     else
