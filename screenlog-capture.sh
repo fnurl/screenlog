0a801c30c05188045f3296ee8f7e57a5d289a50b
Jody Foo <jody.foo@gmail.com>
2015-09-28 10:30:40 +0200

    Added compatibility with dual screen setup and added script for creating movies that have not been created.

--- screenlog-capture.sh
+++ screenlog-capture.sh
@@ -27,13 +27,67 @@ mkdir -p $logpath
 datetime=$(date +%Y%m%d\_%H%M%S)
 prettydatetime=$(date +%Y-%m-%d\ %H:%M:%S)
 
-#/usr/sbin/screencapture -x -t jpg $logpath/$datetime-large.jpg
-screencapture -x -t jpg $logpath/$datetime-large.jpg
-convert $logpath/$datetime-large.jpg -resize 768x480\> $logpath/$datetime-notext.jpg
-rm $logpath/$datetime-large.jpg
 
-convert $logpath/$datetime-notext.jpg -fill white -undercolor "#00000080" \
-    -gravity South -annotate +0+5 "$prettydatetime" -font "Verdana" \
-    -pointsize 64 $logpath/$datetime.jpg
-rm $logpath/$datetime-notext.jpg
-echo "Saved $logpath/$datetime.jpg"
+# TODO: if dspsizes says one screen, do existing code. else add files to the list
+# of files for screencapture to save to
+# then make a big image from all screenshots and resize that
+
+# The command dspsizes outputs multiple lines, so we save each line to an
+# element in the array $dspsizes using `dspsizes=($(dspsizes))`
+#
+# lines are then accessed using:
+# ${dspsizes[0]}
+# ${dspsizes[1]}
+# ${dspsizes[2]}
+dspsizes=($(dspsizes))
+
+# Two displays
+if [ ${#dspsizes[@]} -eq 3 ]; then
+    echo "Capturing two displays.."
+
+    # capture
+    screencapture -x -t jpg $logpath/$datetime-large1.jpg $logpath/$datetime-large2.jpg
+
+    echo "Resizing and adding timestamp.."
+    # resize both screenshots to same height (480), preserving aspect ratio
+    convert $logpath/$datetime-large1.jpg -resize x480\> $logpath/$datetime-h480_1.jpg
+    convert $logpath/$datetime-large2.jpg -resize x480\> $logpath/$datetime-h480_2.jpg
+
+    # tile the two screenshots that have the same height
+    convert $logpath/$datetime-h480_1.jpg $logpath/$datetime-h480_2.jpg\
+            +append $logpath/$datetime-notext.jpg
+
+    # resize tiled image to 768x480 with pillboxing
+    mogrify -resize 768x480 -background black -gravity center -extent 768x480\
+            -format jpg -quality 75 $logpath/$datetime-notext.jpg
+    rm $logpath/$datetime-large1.jpg
+    rm $logpath/$datetime-large2.jpg
+    rm $logpath/$datetime-h480_1.jpg
+    rm $logpath/$datetime-h480_2.jpg
+
+    convert $logpath/$datetime-notext.jpg -fill white -undercolor "#00000080" \
+        -gravity South -annotate +0+5 "$prettydatetime" -font "Verdana" \
+        -pointsize 64 $logpath/$datetime.jpg
+    rm $logpath/$datetime-notext.jpg
+    echo "Saved $logpath/$datetime.jpg"
+
+# One display
+elif [ ${#dspsizes[@]} -eq 2 ]; then
+    echo "Capturing single display.."
+
+    # screenshot
+    screencapture -x -t jpg $logpath/$datetime-large.jpg
+
+    echo "Resizing and adding timestamp.."
+    # resize screenshot to a 768x480 with pillboxing if needed
+    mogrify -resize 768x480 -background black -gravity center -extent 768x480\
+            -format jpg -quality 100 $logpath/$datetime-notext.jpg
+    rm $logpath/$datetime-large.jpg
+
+    # add time stamp
+    convert $logpath/$datetime-notext.jpg -fill white -undercolor "#00000080" \
+        -gravity South -annotate +0+5 "$prettydatetime" -font "Verdana" \
+        -pointsize 64 $logpath/$datetime.jpg
+    rm $logpath/$datetime-notext.jpg
+    echo "Saved $logpath/$datetime.jpg"
+fi
---
+++ screenlog-process-movie-backlog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]; then
+    echo -e "Usage: `basename $0` <path to screenlog dir>\nCreates screenlog movies if they do not exist."
+    exit $E_BADARGS
+fi
+
+logpath=${1%/}
+
+for dir in $logpath/*/
+do
+    dir_noslash=${dir%*/}
+    dir_name=${dir_noslash##*/}
+    #echo raw: $dir, noslash: $dir_noslash, name: $dir_name
+    #echo "Checking if $dir_noslash/$dir_name.mp4 exists.."
+    if [ ! -f $dir_noslash/$dir_name.mp4 ]; then
+        echo "Movie missing for $dir_name, creating..."
+        #create-screenlog-movie.sh -d $dir_name -r /Users/jodfo01/screenlog        
+    fi
+done
